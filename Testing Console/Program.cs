11f440630559cc61717e18b0f70690adcbe40f9e
Francis Zinsli <zinsli.alec@gmail.com>
2021-05-01 22:08:21 -0700

    added ILGPU context and accelerator wrapping class DataScience::GPU

--- Testing Console/Program.cs
+++ Testing Console/Program.cs
@@ -12,20 +12,17 @@ namespace Testing_Console
     {
         static void Main(string[] args)
         {
-            Context context = new Context();
-            Accelerator gpu;
-
-            gpu = Setup.GetGpu(context);
+            GPU gpu = new GPU();
 
             // SAMPLE AND TEST CODE
 
 
-            Vector vectorA = Vector.Linspace(-100, -1, 100);
-            Vector vectorB = Vector.Linspace(-100, 100, 200);
-            Vector vectorC = Vector.Linspace(100000000, 250000000, 25);
-            Vector vectorD = Vector.Linspace(-100000000, -250000000, 25);
-            Vector vectorE = Vector.Linspace(100, -100, 201);
-            Vector vectorF = Vector.Linspace(642.21f, 8412.1f, 55);
+            Vector vectorA = Vector.Linspace(gpu, -100, -1, 100);
+            Vector vectorB = Vector.Linspace(gpu, -100, 100, 200);
+            Vector vectorC = Vector.Linspace(gpu, 100000000, 250000000, 100);
+            Vector vectorD = Vector.Linspace(gpu, -100000000, -250000000, 25);
+            Vector vectorE = Vector.Linspace(gpu, 100, -100, 201);
+            Vector vectorF = Vector.Linspace(gpu, 642.21f, 8412.1f, 55);
 
             //Vector vectorB = Vector.Arange(-10, 10, 5);
 
@@ -44,39 +41,36 @@ namespace Testing_Console
             Console.Write(vectorB.ToCSV());
             Console.WriteLine();
 
-            //Console.WriteLine(string.Join("",Enumerable.Repeat("a",5)));
+            Console.WriteLine(string.Join("", Enumerable.Repeat("a", 5)));
 
-            //Console.Write(vectorD.ToString());
-
-
-            //vectorA.Print();
-
-            //Vector vectorA_absW = Vector.AbsX(gpu, new Vector(new float[] { 1f }));
-
-            //vectorB.Print();
-            //vectorC.Print();
-
-            //vectorB._ConsecutiveOP(gpu, vectorC);
+            Console.Write(vectorD.ToString());
 
 
-            //vectorB.Print();
+            vectorA.Print();
 
-            //Stopwatch sw = new Stopwatch();
-            //sw.Start();
-            //Vector vectorA_abs = Vector.Abs(vectorA);
-            //Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
-            //sw.Stop();
+            Vector vectorA_absW = Vector.AbsX(new Vector(gpu, new float[] { 1f }));
 
-            //sw.Reset();
-            //sw.Start();
-            //Vector vectorA_abs2 = Vector.AbsX(gpu, vectorA);
-            //Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
-            //sw.Stop();
+            vectorB.Print();
+            vectorC.Print();
 
-            //vectorA_abs3.Print();
+            vectorB.Print();
 
-            //Console.WriteLine();
-            //Console.WriteLine($"Minimum in Abs : {vectorA_abs.Value.Min()} \nMinimum in Abs2 : {vectorA_abs2.Value.Min()}");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Vector vectorA_abs = Vector.Abs(vectorA);
+            Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+            sw.Stop();
+
+            sw.Reset();
+            sw.Start();
+            Vector vectorA_abs2 = Vector.AbsX(vectorA);
+            Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+            sw.Stop();
+
+            vectorA_abs2.Print();
+
+            Console.WriteLine();
+            Console.WriteLine($"Minimum in Abs : {vectorA_abs.Value.Min()} \nMinimum in Abs2 : {vectorA_abs2.Value.Min()}");
 
 
 
