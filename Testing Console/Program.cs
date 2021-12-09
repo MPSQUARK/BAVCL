ff86b89d210ed69c99a3465fb43a775baaedc9e1
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2021-12-09 18:27:16 +0000

    Testing Console Updated to .NET6

--- Testing Console/Program.cs
+++ Testing Console/Program.cs
@@ -1,37 +1,18 @@
-﻿using BAVCL;
-//using BAVCL.Core;
-using System;
+﻿using System;
 using BenchmarkDotNet.Running;
 
-using BAVCL.Experimental;
-using BAVCL.Geometric;
+using BAVCL;
 using BAVCL.Core;
+using BAVCL.Geometric;
 using System.Threading.Tasks;
+
 //using BAVCL.Geometric;
 //using ILGPU.Algorithms;
 //using BAVCL.Plotting;
 
-namespace Testing_Console
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            GPU gpu = new GPU();
+GPU gpu = new();
 
+Vector vector = new (gpu, new float[2] { 1f,2f});
 
-            Vector vector = new Vector(gpu, new float[2] { 1f,2f});
+vector.UpdateCache(new float[] { 6f,7f});
 
-            vector.UpdateCache(new float[] { 6f,7f});
-
-        }
-
-
-
-
-
-
-
-    }
-
-}
