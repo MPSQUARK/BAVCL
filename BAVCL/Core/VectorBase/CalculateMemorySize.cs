955f784b0743d5779b03ea5b945f17b2c3c23854
Marcel <marcelpawelczyk7@gmail.com>
2022-11-16 18:41:25 +0000

    added new method to udate the MemorySize Info

--- BAVCL/Core/VectorBase/CalculateMemorySize.cs
+++ BAVCL/Core/VectorBase/CalculateMemorySize.cs
@@ -2,20 +2,14 @@
 
 namespace BAVCL.Core
 {
-    public partial class VectorBase<T>
-    {
+	public partial class VectorBase<T>
+	{
 
-        public long CalculateMemorySize()
-        {
-            return (long)Interop.SizeOf<T>() * (long)this.Value.Length;
-        }
-        public long CalculateMemorySize(T[] array)
-        {
-            return (long)Interop.SizeOf<T>() * (long)array.Length;
-        }
+		public long CalculateMemorySize() => (long)Interop.SizeOf<T>() * (long)this.Value.Length;
+		public long CalculateMemorySize(T[] array) => (long)Interop.SizeOf<T>() * (long)array.Length;
 
-
-    }
-
-
+		public void UpdateMemorySize() => this.MemorySize = CalculateMemorySize();
+		public void UpdateMemorySize(T[] array) => this.MemorySize = CalculateMemorySize(array);
+	
+	}
 }
