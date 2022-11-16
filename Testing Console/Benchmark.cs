6729a0e40d2ce1fa68cbcd3f49445dcb8a157801
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2022-11-16 19:19:41 +0000

    edits

--- Testing Console/Benchmark.cs
+++ Testing Console/Benchmark.cs
@@ -15,146 +15,94 @@ namespace Testing_Console
     [MemoryDiagnoser]
     public class Benchmark
     {
+        //[Params(8, 100)]
+        [Params(2, 8, 16, 49, 100, 64, 256, 529, 165518, 5131, 123, 71645, 12.1518f)]
+        public float val;
 
-        //[Params(2, 8, 16, 49, 100, 64, 256, 529, 165518, 5131, 123, 71645, 12.1518f)]
-        //public float val;
-        public static GPU gpu = new();
-        public static MemoryBuffer1D<float, Stride1D.Dense> input = Vector.Arange(gpu, 0, 1_000_000, 1).GetBuffer();
-        public static int len = (int)input.Length;
+        //public static GPU gpu = new();
+        //public static MemoryBuffer1D<float, Stride1D.Dense> input = Vector.Arange(gpu, 1, 1_000_001, 1).GetBuffer();
+        //public static int len = (int)input.Length;
 
-        public static MemoryBuffer1D<float, Stride1D.Dense> output = Vector.Fill(gpu, 0, len).GetBuffer();
+        //public static MemoryBuffer1D<float, Stride1D.Dense> output = Vector.Fill(gpu, 0, len).GetBuffer();
 
-        public static float[] vals = input.GetAsArray1D();
-        public static float[] _output = new float[len];
+        //public static float[] vals = input.GetAsArray1D();
+        //public static float[] _output = new float[len];
+
+        //[Benchmark]
+        //public void XMathSqrt()
+        //{
+        //    gpu.TestSQRTKernel(gpu.accelerator.DefaultStream, output.IntExtent, output.View, input.View);
+        //    gpu.accelerator.Synchronize();
+        //}
+
+
+        //[Benchmark]
+        //public void MyCbrt()
+        //{
+        //    gpu.TestMYSQRTKernel(gpu.accelerator.DefaultStream, output.IntExtent, output.View, input.View);
+        //    gpu.accelerator.Synchronize();
+        //}
+
+        //[Benchmark]
+        //public void NETLOG2()
+        //{
+        //    Math.Log2(val);
+        //}
+
+        //[Benchmark]
+        //public void MYLOG2()
+        //{
+        //    TestCls.LOG2(val);
+        //}
+
+        //[Benchmark]
+        //public void MYLOG2_V2()
+        //{
+        //    TestCls.LOG2_v2(val);
+        //}
+
+        //[Benchmark]
+        //public void NETLOG10()
+        //{
+        //    Math.Log10(val);
+        //}
+
+        //[Benchmark]
+        //public void MYLOG10()
+        //{
+        //    TestCls.LOG10(val);
+        //}
+
+
+        //[Benchmark]
+        //public void NETCBRT()
+        //{
+        //    Math.Cbrt(val);
+        //}
+
+        //[Benchmark]
+        //public void MY_CBRT_NEW_V2()
+        //{
+        //    TestCls.CBRT_v2(val);
+        //}
 
         [Benchmark]
-        public void XMathSqrt()
+        public void MY_CBRT()
         {
-            gpu.TestSQRTKernel(gpu.accelerator.DefaultStream, output.IntExtent, output.View, input.View);
-            gpu.accelerator.Synchronize();
+            TestCls.CBRT(val);
         }
-
 
         [Benchmark]
-        public void MySqrt()
+        public void MY_CBRT_v2()
         {
-            gpu.TestMYSQRTKernel(gpu.accelerator.DefaultStream, output.IntExtent, output.View, input.View);
-            gpu.accelerator.Synchronize();
+            //TestCls.CBRT_v2(val);
         }
 
-
-        [Benchmark]
-        public void MySQRT()
-        {
-            TestCls.sqrtarr(len, _output, vals);
-        }
-
-
         //[Benchmark]
-        //public void MathSqrt()
+        //public void RUST_CBRT()
         //{
-        //    Math.Sqrt(val);
+        //    TestCls.CbrtRust(val);
         //}
-
-        //[Benchmark]
-        //public void MySqrt()
-        //{
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        TestCls.sqrt_acc_v1(val);
-        //    }
-        //}
-
-        //[Benchmark]
-        //public void MySqrt_v2()
-        //{
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        TestCls.sqrt_acc_v2(val);
-        //    }
-        //}
-
-        //[Benchmark]
-        //public void MySqrt_v3()
-        //{
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        TestCls.sqrt_acc_v3(val);
-        //    }
-        //}
-
-        //[Benchmark]
-        //public void MathSqrt()
-        //{
-        //    Math.Sqrt(val);
-        //}
-
-        //[Benchmark]
-        //public void MySqrt()
-        //{
-        //    TestCls.Sqrt(val);
-        //}
-
-        //[Benchmark]
-        //public void sqrt_simplif_v5()
-        //{
-        //    TestCls.sqrt_simplif_v5(val);
-        //}
-
-        //[Benchmark]
-        //public void sqrt_taylor_v2()
-        //{
-        //    TestCls.sqrt_taylor_v2(val);
-        //}
-
-        //[Benchmark]
-        //public void sqrt_lookup_CPU()
-        //{
-        //    TestCls.sqrt_lookup_CPU(val);
-        //}
-
-        //[Benchmark]
-        //public void sqrt_lookup_Inlined()
-        //{
-        //    TestCls.sqrt_lookup_Inlined(val);
-        //}
-
-        //public static GPU gpu = new GPU();
-
-        //[Params(12,120, 1200,12000,120000,1200000)]
-        //public int length;
-        //public static Vector vector;
-
-
-        //[Benchmark]
-        //public void ToStr()
-        //{
-        //    vector = Vector.Arange(gpu, -12, length, 1, 1, false);
-        //    vector.ToString();
-        //}
-
-        //[Benchmark]
-        //public void ToStrNewer()
-        //{
-        //    vector = Vector.Arange(gpu, -12, length, 1, 1, false);
-        //    vector.ToStringNEWER();
-        //}
-
-        //[Benchmark]
-        //public void ToStrNewer2()
-        //{
-        //    vector = Vector.Arange(gpu, -12, length, 1, 1, false);
-        //    vector.ToStringNEWER2();
-        //}
-
-        //[Benchmark]
-        //public void ToStrNEWEST()
-        //{
-        //    vector = Vector.Arange(gpu, -12, length, 1, 1, false);
-        //    vector.ToStringNEWEST();
-        //}
-
-
 
     }
 
--- Testing Console/Program.cs
+++ Testing Console/Program.cs
@@ -1,16 +1,111 @@
 ﻿using System;
 using BenchmarkDotNet.Running;
-
 using BAVCL;
 using BAVCL.Core;
 using BAVCL.Geometric;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using Testing_Console;
+using BAVCL.Experimental;
 
 
 GPU gpu = new();
+Vector vec = Vector.Arange(gpu,0f, 20000000f, 0.5f,5);
 
-Vector vector = new (gpu, new float[2] { 1f,2f});
+vec.Print();
 
-vector.UpdateCache(new float[] { 6f,7f});
 
-vector.Print();
+Console.WriteLine(TestCls.Sqrt(311.3f));
+Console.ReadLine();
+
+
+
+
+//double[] arr = new double[2000000];
+//Random rnd = new Random();
+
+//for (int i = 0; i < arr.Length; i+=2)
+//{
+//    arr[i] = rnd.Next(2,(int)1e8) + rnd.NextDouble();
+//    arr[i+1] = Math.Cbrt(arr[i]);
+//}
+
+//double[] acc = new double[8];
+
+//for (int i = 0; i < arr.Length; i+=2)
+//{
+//    acc[1] += Math.Abs(TestCls.CBRT_FAST(arr[i]) - arr[i + 1]);
+//    acc[3] += Math.Abs(TestCls.CBRT_v2(arr[i]) - arr[i + 1]);
+//    acc[5] += Math.Abs(TestCls.CbrtRust(arr[i]) - arr[i + 1]);
+//    acc[7] += Math.Abs(Math.Cbrt(arr[i]) - arr[i + 1]);
+//}
+//Console.WriteLine();
+//for (int i = 0; i < acc.Length; i++)
+//{
+//    Console.Write($"{acc[i]}, ");
+
+//}
+
+//Console.WriteLine($"CBRT v3:  {TestCls.CBRT_v3(124512)}");
+//Console.WriteLine($"CBRT_FAST {TestCls.CBRT_FAST(124512)}");
+//Console.WriteLine($"CbrtFast  {TestCls.CbrtFast(124512)}");
+//Console.WriteLine($"CBRT_v2   {TestCls.CBRT_v2(124512)}");
+//Console.WriteLine($"CBRT      {TestCls.CBRT(124512)}");
+//Console.WriteLine($"CbrtRust  {TestCls.CbrtRust(124512)}");
+//Console.WriteLine($"Math      {Math.Cbrt(124512)}");
+//Console.WriteLine($"Actual    {49.934848475784521335244675506973788632625373546702390337469711516}");
+
+//TestCls.TSTCODE();
+
+//TestCls.LOG2(7f).Print(12);
+//TestCls.LOG2_v2(7f).Print(12);
+//Math.Log2(7f).Print(12);
+
+BenchmarkRunner.Run<Benchmark>();
+
+
+
+Console.ReadLine();
+
+//GPU gpu = new();
+
+//float[] vector = Vector.Arange(1f, 500000f, 0.1f);
+
+//float[] _math = new float[vector.Length];
+//float[] _cbrt_new = new float[vector.Length];
+//float[] _cbrt_original = new float[vector.Length];
+
+
+//for (int i = 0; i < vector.Length; i++)
+//{
+//    _math[i] = MathF.Cbrt(vector[i]);
+//    _cbrt_new[i] = TestCls.CBRT(vector[i]);
+//    _cbrt_original[i] = TestCls.CbrtFast(vector[i]);
+//}
+
+//Vector vecMath = new(gpu, _math, 10);
+//Vector vecNEW = new(gpu, _cbrt_new, 10);
+//Vector vecOrig = new(gpu, _cbrt_original, 10);
+
+
+//Vector newError = vecMath - vecNEW;
+//Vector origError = vecMath - vecOrig;
+
+
+//Console.WriteLine("Original METHOD:");
+//Console.WriteLine($"MIN: {origError.Min()}");
+//Console.WriteLine($"MAX: {origError.Max()}");
+//Console.WriteLine($"AVG: {origError.Mean()}");
+
+//Console.WriteLine("NEW METHOD:");
+//Console.WriteLine($"MIN: {newError.Min()}");
+//Console.WriteLine($"MAX: {newError.Max()}");
+//Console.WriteLine($"AVG: {newError.Mean()}");
+
+
+//float value = 58135183131f;
+//Console.WriteLine();
+//Console.WriteLine(MathF.Cbrt(value));
+//Console.WriteLine(Math.Cbrt(value));
+//Console.WriteLine(TestCls.CBRT(value));
