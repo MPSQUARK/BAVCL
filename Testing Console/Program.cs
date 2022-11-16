7ea69b39405ac613ade4d0c876229166767d27cc
Marcel <marcelpawelczyk7@gmail.com>
2022-11-16 19:14:09 +0000

    added benchmark code

--- Testing Console/Program.cs
+++ Testing Console/Program.cs
@@ -5,12 +5,10 @@ using BAVCL;
 using BAVCL.Core;
 using BAVCL.Geometric;
 using System.Threading.Tasks;
-
+using Testing_Console;
 
 GPU gpu = new();
 
 Vector vector = new (gpu, new float[2] { 1f,2f});
 
-vector.UpdateCache(new float[] { 6f,7f});
-
-vector.Print();
+BenchmarkRunner.Run<Benchmark>();
