7422201fba1aa6e9edb6a063c34a282dba86b332
Marcel <marcelpawelczyk7@gmail.com>
2022-11-16 18:43:27 +0000

    refactoring changes - readability

--- BAVCL/Core/VectorBase/VectorBase.cs
+++ BAVCL/Core/VectorBase/VectorBase.cs
@@ -4,103 +4,85 @@ using System.Linq;
 
 namespace BAVCL.Core
 {
-    public abstract partial class VectorBase<T> : ICacheable, IIO where T : unmanaged 
-    {
-        protected GPU gpu;
+	public abstract partial class VectorBase<T> : ICacheable, IIO where T : unmanaged 
+	{
+		protected GPU gpu;
 
-        public T[] Value;
+		public T[] Value;
 
-        public virtual int Columns
-        {
-            get { return _columns; }
-            set { _columns = value > 0 ? value : throw new Exception($"Columns must be a positive integer greater than zero. Recieved {value}"); }
-        }
-        public virtual int Length 
-        { 
-            get { return _length; } 
-            set { _length = value; } 
-        }
-        public uint ID
-        {
-            get { return _id; }
-            set { _id = Math.Clamp(value, 0, uint.MaxValue); }
-        }
-        public long MemorySize 
-        { 
-            get { return _memorySize; } 
-            set { _memorySize = value; } 
-        }
-        public uint LiveCount 
-        { 
-            get { return _livecount; } 
-            set { _livecount = value; } 
-        }
+		public virtual int Columns
+		{
+			get => _columns;
+			set { _columns = value > 0 ? value : throw new Exception($"Columns must be a positive integer greater than zero. Recieved {value}"); }
+		}
+		public virtual int Length 
+		{ 
+			get => _length; 
+			set => _length = value; 
+		}
+		public uint ID
+		{
+			get => _id;
+			set => _id = Math.Clamp(value, 0, uint.MaxValue);
+		}
+		public long MemorySize 
+		{ 
+			get => _memorySize; 
+			set => _memorySize = value; 
+		}
+		public uint LiveCount 
+		{ 
+			get => _livecount; 
+			set => _livecount = value; 
+		}
 
-        protected internal int _columns = 1;
-        protected internal int _length = 0;
-        protected internal uint _id = 0;
-        protected internal long _memorySize = 0;
-        protected internal uint _livecount = 0;
-
-        
-
-        protected VectorBase(GPU gpu, T[] value, int columns = 1, bool Cache = true)
-        {
-            this.gpu = gpu;
-            this.Columns = columns;
-            if (Cache)
-            {
-                this.Cache(value);
-                return;
-            }
-            this.Value = value;
-            this.Length = value.Length;
-            this._memorySize = this.CalculateMemorySize();
-        }
+		protected internal int _columns = 1;
+		protected internal int _length = 0;
+		protected internal uint _id = 0;
+		protected internal long _memorySize = 0;
+		protected internal uint _livecount = 0;
 
 
+		protected VectorBase(GPU gpu, T[] value, int columns = 1, bool Cache = true)
+		{
+			this.gpu = gpu;
+			this.Columns = columns;
+			this.Value = value;
+			this.Length = value.Length;
+			this.UpdateMemorySize();
+			
+			if (Cache) this.Cache(value);
+		}
 
-        public T[] Pull()
-        {
-            return GetBuffer().GetAsArray1D();
-        }
+		public T[] Pull() => GetBuffer().GetAsArray1D();
 
-        public T GetValue(int row, int col)
-        {
-            SyncCPU();
-            return this.Value[row * this.Columns + col];
-        }
+		public T GetValue(int row, int col)
+		{
+			SyncCPU();
+			return this.Value[row * this.Columns + col];
+		}
 
-        // PRINT + CSV
-        public virtual void Print()
-        {
-            Console.WriteLine(this.ToString());
-            return;
-        }
+		// PRINT + CSV
+		public virtual void Print() => Console.WriteLine(this.ToString());
 
-        // MATHEMATICAL PROPERTIES 
-        public int RowCount()
-        {
-            if (Columns == 1) { return 1; }
-            return _length / Columns;
-        }
+		// MATHEMATICAL PROPERTIES 
+		public int RowCount()
+		{
+			if (Columns == 1) return 1;
+			return _length / Columns;
+		}
 
-        public virtual (int, int) Shape()
-        {
-            return (RowCount(), Columns);
-        }
+		public virtual (int, int) Shape() => (RowCount(), Columns);
 
-        public virtual T Max() { SyncCPU(); return Value.Max(); }
-        public virtual T Min() { SyncCPU(); return Value.Min();}
-        public abstract T Mean();
-        public abstract T Range();
-        public abstract T Sum();
-        public bool IsRectangular()
-        {
-            return (this.Length % this.Columns == 0);
-        }
+		public virtual T Max() { SyncCPU(); return Value.Max(); }
+		public virtual T Min() { SyncCPU(); return Value.Min();}
+		public abstract T Mean();
+		public abstract T Range();
+		public abstract T Sum();
+		public bool IsRectangular() => this.Length % this.Columns == 0;
 
-    }
+
+	}
 
 
 }
