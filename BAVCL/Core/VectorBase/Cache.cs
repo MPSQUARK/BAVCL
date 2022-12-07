37a7427686b67d6b95bdf78a724400cab9e85296
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2022-12-07 23:21:50 +0000

    Updating MemorySize and Length is no longer nessessary when caching

--- BAVCL/Core/VectorBase/Cache.cs
+++ BAVCL/Core/VectorBase/Cache.cs
@@ -3,19 +3,13 @@
 	public partial class VectorBase<T>
 	{
 		internal void Cache()
-		{
-			this.UpdateMemorySize();
-			this.Length = this.Value.Length;
-			
+		{	
 			// Store info about data to LRU
 			this.ID = this.gpu.Allocate(this, this.Value);
 		}
 
 		internal void Cache(T[] array)
 		{
-			this.MemorySize = this.CalculateMemorySize(array);
-			this.Length = array.Length;
-
 			// Store info about data to LRU
 			this.ID = this.gpu.Allocate(this, array);
 		}
