74edeefae0f5bd2322a7ca1898dfe9ea51645992
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2023-01-22 22:32:45 +0000

    added Cache struct to unify the cache data

---
+++ BAVCL/Core/GPU/Cache.cs
@@ -0,0 +1,21 @@
+﻿using BAVCL.Core;
+using ILGPU.Runtime;
+using System;
+
+
+namespace BAVCL
+{
+    public struct Cache
+    {
+        public MemoryBuffer MemoryBuffer;
+        public WeakReference<ICacheable> CachedObjRef;
+        public uint LiveCount;
+
+        public Cache(MemoryBuffer memoryBuffer, WeakReference<ICacheable> cachedObjRef)
+        {
+            MemoryBuffer = memoryBuffer;
+            CachedObjRef = cachedObjRef;
+            LiveCount = 0;
+        }
+    }
+}
