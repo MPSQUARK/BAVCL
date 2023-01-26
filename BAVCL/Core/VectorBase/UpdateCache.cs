64e0f078f7255a2f7988027303a3f4f8c14b4752
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2023-01-26 16:07:07 +0000

    updateCache updated and moved to the GPU end

--- BAVCL/Core/VectorBase/UpdateCache.cs
+++ BAVCL/Core/VectorBase/UpdateCache.cs
@@ -6,87 +6,18 @@ namespace BAVCL.Core
 {
     public partial class VectorBase<T>
     {
-        public void UpdateCache()
+        public MemoryBuffer UpdateCache()
         {
-            if (this._id == 0) 
-            { 
-                Cache(); 
-                return; 
-            }
+            if (ID == 0) return Cache();
 
-            // If the ID does not exist in GPU's Cached Memory
-            MemoryBuffer Data;
-            if (!gpu.GPUbuffers.TryGetValue(this._id, out Data))
-            {
-                // Try remove this ID from weakReferences
-                this.gpu.CPUrefs.TryRemove(this._id, out _);
-
-                // Cache the Data
-                Cache();
-                return;
-            }
-
-
-            // If the Lengths don't match remove old data and cache again
-            if (Data.Length != Value.Length)
-            {
-                this._id = this.gpu.DeCache(this._id);
-                Cache();
-                return;
-            }
-
-            // Else if the lengths match update the cache
-
-            // Convert Buffer Data to that of this type
-            MemoryBuffer1D<T, Stride1D.Dense> data = (MemoryBuffer1D<T, Stride1D.Dense>)Data;
-
-            // Copy new data to buffer 
-            data.CopyFromCPU(Value);
-            if (this.gpu.GPUbuffers.TryUpdate(this._id, data, Data)) { return; }
-
-
-            throw new Exception("Unexpected ERROR in UpdateCache");
+            ID = gpu.GCItem(ID);
+            return Cache();
         }
 
-        public void UpdateCache(T[] array)
+        public MemoryBuffer UpdateCache(T[] array)
         {
-            if (this._id == 0) { Cache(array); return; }
-
-            // If the ID does not exist in GPU's Cached Memory
-            MemoryBuffer Data;
-            if (!gpu.GPUbuffers.TryGetValue(this._id, out Data))
-            {
-                // Try remove this ID from weakReferences
-                this.gpu.CPUrefs.TryRemove(this._id, out _);
-
-                // Cache the Data
-                Cache();
-                return;
-            }
-
-
-            // If the Lengths don't match remove old data and cache again
-            if (Data.Length != array.Length)
-            {
-                this._id = this.gpu.DeCache(this._id);
-                Cache(array);
-                return;
-            }
-
-            // Else if the lengths match update the cache
-
-            // Convert Buffer Data to that of this type
-            MemoryBuffer1D<T, Stride1D.Dense> data = (MemoryBuffer1D<T, Stride1D.Dense>)Data;
-
-            // Copy new data to buffer 
-            data.CopyFromCPU(array);
-            if (this.gpu.GPUbuffers.TryUpdate(_id, data, Data)) { return; }
-
-
-            this._id = this.gpu.DeCache(this._id);
-            Cache(array);
-            return;
-            //throw new Exception("Unexpected ERROR in UpdateCache");
+            (ID, MemoryBuffer buffer) = gpu.UpdateBuffer(this, array);
+            return buffer;
         }
 
 
