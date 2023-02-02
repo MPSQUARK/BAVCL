41261171a510d37df9e95fb22059b2947e95aba4
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2023-02-02 21:28:06 +0000

    added annotation

--- BAVCL/Core/VectorBase/GetBuffer.cs
+++ BAVCL/Core/VectorBase/GetBuffer.cs
@@ -5,7 +5,10 @@ namespace BAVCL.Core
 {
     public partial class VectorBase<T>
     {
-
+        /// <summary>
+        /// NOTE: This does NOT update the Length property
+        /// </summary>
+        /// <returns></returns>
         public MemoryBuffer1D<T, Stride1D.Dense> GetBuffer() =>
             (MemoryBuffer1D<T, Stride1D.Dense>)(gpu.TryGetBuffer<T>(ID) ?? Cache()); 
 
