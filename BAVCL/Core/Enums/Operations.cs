7285db8f0675b8545fd330ca77cc6285e6c59229
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2023-10-22 14:51:22 +0100

    added distance operation

--- BAVCL/Core/Enums/Operations.cs
+++ BAVCL/Core/Enums/Operations.cs
@@ -1,15 +1,16 @@
 ﻿namespace BAVCL
 {
-    public enum Operations
-    {
-        multiply = 0,
-        add = 1,
-        subtract = 2,
-        divide = 3,
-        pow = 4,
-        flipDivide = 5,
-        flipSubtract = 6,
-        flipPow = 7,
-        differenceSquared = 8, // square the difference of two values
-    }
+	public enum Operations
+	{
+		multiply = 0,
+		add = 1,
+		subtract = 2,
+		divide = 3,
+		pow = 4,
+		flipDivide = 5,
+		flipSubtract = 6,
+		flipPow = 7,
+		differenceSquared = 8, // square the difference of two values
+		distance = 9, // square root of the difference squared
+	}
 }
