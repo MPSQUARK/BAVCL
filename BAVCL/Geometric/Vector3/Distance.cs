be2c5b2e0a93397967b3a5668b738f9e7d5a4656
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2023-10-22 15:10:02 +0100

    Distance Added

---
+++ BAVCL/Geometric/Vector3/Distance.cs
@@ -0,0 +1,17 @@
+using System;
+using BAVCL.Core;
+
+namespace BAVCL.Geometric;
+
+public sealed partial class Vector3 : VectorBase<float>
+{
+	public static Vector Distance(Vector3 vectorA, Vector3 vectorB)
+	{
+		if (vectorA.Length != vectorB.Length) 
+		{ 
+			throw new Exception($"Cannot Cross Product two Vector3's together of different lengths. {vectorA.Length} != {vectorB.Length}"); 
+		}
+		
+		return VOP(vectorA, vectorB, Operations.distance);
+	}
+}
