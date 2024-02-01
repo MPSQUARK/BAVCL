f7e974d3dfe55c776b8f7269cc8a3231a707da23
Marcel <marcelpawelczyk7@gmail.com>
2024-02-01 20:41:36 +0000

    filescoped namespace

--- Source/Extensions/Max.cs
+++ Source/Extensions/Max.cs
@@ -1,37 +1,35 @@
 ﻿using System;
 using System.Numerics;
 
-namespace BAVCL.Ext
+namespace BAVCL.Ext;
+
+public static partial class Extensions
 {
-	public static partial class Extensions
+	public static T Max<T>(this T[] arr) where T : INumber<T>
 	{
-		public static T Max<T>(this T[] arr) where T : INumber<T>
-		{
-			if (arr.Length == 0) throw new Exception("Cannot Be Length 0");
-			T max = arr[0];
-			for (int i = 1; i < arr.Length; i++)
-				if (max < arr[i])
-					max = arr[i];
-			return max;
-		}
-	
-		public static T Max<T>(this T[] arr, bool includeInfinity=true) where T : INumber<T>
-		{
-			if (includeInfinity) return arr.Max();
-
-			if (arr.Length == 0) throw new Exception("Cannot Be Length 0");
-
-			T max = arr[0];
-
-			for (int i = 1; i < arr.Length; i++)
-			{
-				if (T.IsInfinity(arr[i])) continue;
-				if (max < arr[i])
-					max = arr[i];
-			}
-
-			return max;
-		}	
+		if (arr.Length == 0) throw new Exception("Cannot Be Length 0");
+		T max = arr[0];
+		for (int i = 1; i < arr.Length; i++)
+			if (max < arr[i])
+				max = arr[i];
+		return max;
 	}
 
+	public static T Max<T>(this T[] arr, bool includeInfinity = true) where T : INumber<T>
+	{
+		if (includeInfinity) return arr.Max();
+
+		if (arr.Length == 0) throw new Exception("Cannot Be Length 0");
+
+		T max = arr[0];
+
+		for (int i = 1; i < arr.Length; i++)
+		{
+			if (T.IsInfinity(arr[i])) continue;
+			if (max < arr[i])
+				max = arr[i];
+		}
+
+		return max;
+	}
 }
