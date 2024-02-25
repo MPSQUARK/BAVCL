34e73831804d6682817e9adf1aaba21c4e123758
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2024-02-25 14:50:05 +0000

    added hints to know what the flipOps do

--- Source/BAVCL/Enums/Operations.cs
+++ Source/BAVCL/Enums/Operations.cs
@@ -4,11 +4,29 @@ public enum Operations
 {
 	multiply = 0,
 	add = 1,
+	/// <summary>
+	/// Vector - Scalar
+	/// </summary>	
 	subtract = 2,
+	/// <summary>
+	/// Vector - Scalar
+	/// </summary>	
 	divide = 3,
+	/// <summary>
+	/// Vector - Scalar
+	/// </summary>	
 	pow = 4,
+	/// <summary>
+	/// Scalar - Vector
+	/// </summary>
 	flipDivide = 5,
+	/// <summary>
+	/// Scalar - Vector
+	/// </summary>
 	flipSubtract = 6,
+	/// <summary>
+	/// Scalar - Vector
+	/// </summary>
 	flipPow = 7,
 	differenceSquared = 8, // square the difference of two values
 	distance = 9, // square root of the difference squared
