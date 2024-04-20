2e231d5714cfa66070518abe012f34edfbe36900
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2024-04-20 13:46:28 +0100

    added arange generator

--- Source/Core/Vec/Arange.cs
+++ Source/Core/Vec/Arange.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Numerics;
 using BAVCL.MemoryManagement;
 
@@ -5,33 +7,51 @@ namespace BAVCL;
 
 public partial class Vec<T> : ICacheable<T> where T : unmanaged, INumber<T>
 {
-    // public static Vec<T> Arange(GPU gpu, T startval, T endval, T interval, int Columns = 1, bool cache = true)
-    // {
-    // 	int steps = (int)((endval - startval) / interval);
-    // 	if (endval < startval && interval > 0) { steps = XMath.Abs(steps); interval = -interval; }
-    // 	if (endval % interval != 0) { steps++; }
+	// 	public static Vec<T> Arange(GPU gpu, T startval, T endval, T interval, int Columns = 1, bool cache = true)
+	// 	{
+	// 		int steps = (int)((endval - startval) / interval);
+	// 		if (endval < startval && interval > 0) { steps = XMath.Abs(steps); interval = -interval; }
+	// 		if (endval % interval != 0) { steps++; }
 
-    // 	float[] values = new float[steps];
+	// 		float[] values = new float[steps];
 
-    // 	for (int i = 0; i < steps; i++)
-    // 		values[i] = startval + (i * interval);
-
-
-    // 	return new Vec<T>(gpu, values, Columns, cache);
-    // }
+	// 		for (int i = 0; i < steps; i++)
+	// 			values[i] = startval + (i * interval);
 
 
-    // public static float[] Arange(float startval, float endval, float interval)
-    // {
-    // 	int steps = (int)((endval - startval) / interval);
-    // 	if (endval < startval && interval > 0) { steps = XMath.Abs(steps); interval = -interval; }
-    // 	if (endval % interval != 0) { steps++; }
+	// 		return new Vec<T>(gpu, values, Columns, cache);
+	// 	}
 
-    // 	float[] values = new float[steps];
+	public static IEnumerable<T> ArangeGenerator(T startValue, T endValue, T interval)
+	{
+		if (endValue < startValue && interval > T.Zero)
+			interval = -interval;
 
-    // 	for (int i = 0; i < steps; i++)
-    // 		values[i] = startval + (i * interval);
+		if (interval > T.Zero)
+		{
+			for (T i = startValue; i < endValue; i += interval)
+				yield return i;
+		}
+		else if (interval < T.Zero)
+		{
+			for (T i = startValue; i > endValue; i += interval)
+				yield return i;
+		}
+	}
 
-    // 	return values;
-    // }
+
+
+	// public static float[] Arange(float startval, float endval, float interval)
+	// {
+	// 	int steps = (int)((endval - startval) / interval);
+	// 	if (endval < startval && interval > 0) { steps = XMath.Abs(steps); interval = -interval; }
+	// 	if (endval % interval != 0) { steps++; }
+
+	// 	float[] values = new float[steps];
+
+	// 	for (int i = 0; i < steps; i++)
+	// 		values[i] = startval + (i * interval);
+
+	// 	return values;
+	// }
 }
