8c96575f407477a53163a217efe1a64b07989e4d
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2024-04-26 21:02:49 +0100

    Ported diff func

--- Source/Core/Vec/Copy.cs
+++ Source/Core/Vec/Copy.cs
@@ -8,8 +8,7 @@ public partial class Vec<T> : ICacheable<T> where T : unmanaged, INumber<T>
     public Vec<T> Copy(bool Cache = true)
     {
         if (ID == 0)
-            return new Vec<T>(Gpu, Values[..], Columns, Cache);
-
+            return new Vec<T>(Gpu, Values, Columns, Cache);
         return new Vec<T>(Gpu, Pull(), Columns, Cache);
     }
 }
