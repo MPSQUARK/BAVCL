a4a82df4e7ada65bace4b9e373b4fc79d6bb0a02
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2024-04-28 19:25:05 +0100

    added linspace

--- Source/Extensions/Generators.cs
+++ Source/Extensions/Generators.cs
@@ -5,20 +5,27 @@ namespace BAVCL.Ext;
 
 public static class Generators
 {
-    public static IEnumerable<T> Arange<T>(T startValue, T endValue, T interval) where T : INumber<T>
-    {
-        if (endValue < startValue && interval > T.Zero)
-            interval = -interval;
+	public static IEnumerable<T> Arange<T>(T startValue, T endValue, T interval) where T : INumber<T>
+	{
+		if (endValue < startValue && interval > T.Zero)
+			interval = -interval;
 
-        if (interval > T.Zero)
-        {
-            for (T i = startValue; i < endValue; i += interval)
-                yield return i;
-        }
-        else if (interval < T.Zero)
-        {
-            for (T i = startValue; i > endValue; i += interval)
-                yield return i;
-        }
-    }
+		if (interval > T.Zero)
+		{
+			for (T i = startValue; i < endValue; i += interval)
+				yield return i;
+		}
+		else if (interval < T.Zero)
+		{
+			for (T i = startValue; i > endValue; i += interval)
+				yield return i;
+		}
+	}
+
+	public static IEnumerable<T> Linspace<T>(T startValue, T endValue, int num) where T : INumber<T>
+	{
+		T interval = (endValue - startValue) / T.CreateChecked(num);
+		return Arange(startValue, endValue, interval);
+	}
+
 }
