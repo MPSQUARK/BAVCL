873d337cfa04a4dc55d0546831b8999838d3b95b
marcel <marcelpawelczyk7@gmail.com>
2025-12-06 19:54:32 +0000

    refactor

--- BAVCL/Core/Vector/Arange.cs
+++ BAVCL/Core/Vector/Arange.cs
@@ -7,16 +7,7 @@ namespace BAVCL
 
         public static Vector Arange(GPU gpu, float startval, float endval, float interval, int Columns = 1, bool cache = true)
         {
-            int steps = (int)((endval - startval) / interval);
-            if (endval < startval && interval > 0) { steps = XMath.Abs(steps); interval = -interval; }
-            if (endval % interval != 0) { steps++; }
-
-            float[] values = new float[steps];
-
-            for (int i = 0; i < steps; i++)
-                values[i] = startval + (i * interval);
-
-
+            float[] values = Arange(startval, endval, interval);
             return new Vector(gpu, values, Columns, cache);
         }
 
