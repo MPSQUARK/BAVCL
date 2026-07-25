55c1878eec110e57d4ad6bea592fd9ac9a2a3df5
marcel <marcelpawelczyk7@gmail.com>
2026-07-25 14:01:12 +0100

    test

--- Testing Console/Program.cs
+++ Testing Console/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using BAVCL;
 
 int itter = 100000;
@@ -8,14 +9,15 @@ GPU gpu = GPUManager.Default;
 
 Stopwatch sw = Stopwatch.StartNew();
 
-Vector vec = new(gpu, [1, 2, 3, 4, 5, 6], 1);
-Vector vec2 = new(gpu, [5, 5, 5, 5, 5, 5], 1);
+Vector vec = new(gpu, [1, 2, 3, 4, 5, 6]);
+Vector vec2 = new(gpu, [5, 5, 5, 5, 5, 5]);
 
-for (int i = 0; i < itter; i++)
+Parallel.For(0, itter, i =>
 {
     Vector vec3 = vec * vec2;
     Vector vec4 = vec2 * vec;
 }
+);
 
 var time = sw.ElapsedMilliseconds;
 Console.WriteLine($"Time taken: {(float)time/(float)itter} ms (total: {time} ms)");
