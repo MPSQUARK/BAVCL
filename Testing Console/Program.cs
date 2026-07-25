a852ab5985b576a16f5f6b8d132613e35e89c292
marcel <marcelpawelczyk7@gmail.com>
2026-07-25 13:33:40 +0100

    --

--- Testing Console/Program.cs
+++ Testing Console/Program.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Diagnostics;
 using BAVCL;
 using BAVCL.Modules.Arithmetic;
 
 GPU gpu = GPUManager.Default;
+
+Stopwatch sw = Stopwatch.StartNew();
+
 Vector vec = new(gpu, [1, 2, 3, 4, 5, 6], 2);
 Vector vec2 = new(gpu, [5, 5, 5, 5, 5, 5], 3);
 
@@ -13,3 +18,6 @@ Vector vec4 = vec2.Cross(vec);
 Vector.Cross(vec, vec2).Print();
 vec3.Print();
 vec4.Print();
+
+var time = sw.ElapsedMilliseconds;
+Console.WriteLine($"Time taken: {time} ms");
