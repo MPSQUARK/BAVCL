93e94021e87a78b3fa443a871736a6b88b1558fd
marcel <marcelpawelczyk7@gmail.com>
2026-07-31 19:44:09 +0100

    comments added

--- Source/Core/Enums/Operations.cs
+++ Source/Core/Enums/Operations.cs
@@ -6,11 +6,32 @@ public enum Operations
 	add = 1,
 	subtract = 2,
 	divide = 3,
+	/// <summary>
+	/// Vector ^ Scalar
+	/// </summary>
 	pow = 4,
+	/// <summary>
+	/// Scalar / Vector
+	/// </summary>
 	flipDivide = 5,
+	/// <summary>
+	/// Scalar - Vector
+	/// </summary>
 	flipSubtract = 6,
+	/// <summary>
+	/// Scalar ^ Vector
+	/// </summary>
 	flipPow = 7,
+	/// <summary>
+	/// (a - b)^2
+	/// </summary>
 	differenceSquared = 8, // square the difference of two values
+	/// <summary>
+	/// Sqrt((a - b)^2)
+	/// </summary>
 	distance = 9, // square root of the difference squared
+	/// <summary>
+	/// Sqrt(a^2 + b^2)
+	/// </summary>
 	magnitude = 10, // square root of the sum of the squares
 }
