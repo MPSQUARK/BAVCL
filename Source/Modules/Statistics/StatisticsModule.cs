af8dfac5ff9470a8b2b8b656d9465c333092a69d
marcel <marcelpawelczyk7@gmail.com>
2026-08-15 23:47:17 +0100

    stats module

--- Source/Modules/Statistics/StatisticsModule.cs
+++ Source/Modules/Statistics/StatisticsModule.cs
@@ -102,6 +102,42 @@ public static class VectorStatistics
 
 		public static bool All(Vector vector) => DescriptiveStatistics.All(vector);
 
+		public static float MeanX(Vector vector) => GpuDescriptiveStatistics.MeanX(vector);
+
+		public static float VarX(Vector vector) => GpuDescriptiveStatistics.VarX(vector);
+
+		public static float StdX(Vector vector) => GpuDescriptiveStatistics.StdX(vector);
+
+		public static float MinX(Vector vector) => GpuDescriptiveStatistics.MinX(vector);
+
+		public static float MaxX(Vector vector) => GpuDescriptiveStatistics.MaxX(vector);
+
+		public static float RangeX(Vector vector) => GpuDescriptiveStatistics.RangeX(vector);
+
+		public static bool AllX(Vector vector) => GpuDescriptiveStatistics.AllX(vector);
+
+		public static float Percentile(Vector vector, float percentile) =>
+			OrderStatistics.Percentile(vector, percentile);
+
+		public static float Median(Vector vector) => OrderStatistics.Median(vector);
+
+		public static float Quartile1(Vector vector) => OrderStatistics.Quartile1(vector);
+
+		public static float Quartile3(Vector vector) => OrderStatistics.Quartile3(vector);
+
+		public static float Iqr(Vector vector) => OrderStatistics.Iqr(vector);
+
+		public static float PercentileX(Vector vector, float percentile) =>
+			GpuOrderStatistics.PercentileX(vector, percentile);
+
+		public static float MedianX(Vector vector) => GpuOrderStatistics.MedianX(vector);
+
+		public static float Quartile1X(Vector vector) => GpuOrderStatistics.Quartile1X(vector);
+
+		public static float Quartile3X(Vector vector) => GpuOrderStatistics.Quartile3X(vector);
+
+		public static float IqrX(Vector vector) => GpuOrderStatistics.IqrX(vector);
+
 		public static Vector ReduceOPX(Vector vector, Vector matrix, Operations operation) =>
 			ReduceCore.ReduceOPX(vector, matrix, operation);
 	}
@@ -136,6 +172,42 @@ public static class VectorStatistics
 		public static int Range(VectorInt vector) => DescriptiveStatistics.Range(vector);
 
 		public static bool All(VectorInt vector) => DescriptiveStatistics.All(vector);
+
+		public static float MeanX(VectorInt vector) => GpuDescriptiveStatistics.MeanX(vector);
+
+		public static float VarX(VectorInt vector) => GpuDescriptiveStatistics.VarX(vector);
+
+		public static float StdX(VectorInt vector) => GpuDescriptiveStatistics.StdX(vector);
+
+		public static int MinX(VectorInt vector) => GpuDescriptiveStatistics.MinX(vector);
+
+		public static int MaxX(VectorInt vector) => GpuDescriptiveStatistics.MaxX(vector);
+
+		public static int RangeX(VectorInt vector) => GpuDescriptiveStatistics.RangeX(vector);
+
+		public static bool AllX(VectorInt vector) => GpuDescriptiveStatistics.AllX(vector);
+
+		public static float Percentile(VectorInt vector, float percentile) =>
+			OrderStatistics.Percentile(vector, percentile);
+
+		public static float Median(VectorInt vector) => OrderStatistics.Median(vector);
+
+		public static float Quartile1(VectorInt vector) => OrderStatistics.Quartile1(vector);
+
+		public static float Quartile3(VectorInt vector) => OrderStatistics.Quartile3(vector);
+
+		public static float Iqr(VectorInt vector) => OrderStatistics.Iqr(vector);
+
+		public static float PercentileX(VectorInt vector, float percentile) =>
+			GpuOrderStatistics.PercentileX(vector, percentile);
+
+		public static float MedianX(VectorInt vector) => GpuOrderStatistics.MedianX(vector);
+
+		public static float Quartile1X(VectorInt vector) => GpuOrderStatistics.Quartile1X(vector);
+
+		public static float Quartile3X(VectorInt vector) => GpuOrderStatistics.Quartile3X(vector);
+
+		public static float IqrX(VectorInt vector) => GpuOrderStatistics.IqrX(vector);
 
 		public static VectorInt ReduceOPX(VectorInt vector, VectorInt matrix, Operations operation) =>
 			ReduceCore.ReduceOPX(vector, matrix, operation);
@@ -237,6 +309,40 @@ public static class StatisticsModule
 
 		public bool All() => VectorStatistics.All(vector);
 
+		public float MeanX() => VectorStatistics.MeanX(vector);
+
+		public float VarX() => VectorStatistics.VarX(vector);
+
+		public float StdX() => VectorStatistics.StdX(vector);
+
+		public float MinX() => VectorStatistics.MinX(vector);
+
+		public float MaxX() => VectorStatistics.MaxX(vector);
+
+		public float RangeX() => VectorStatistics.RangeX(vector);
+
+		public bool AllX() => VectorStatistics.AllX(vector);
+
+		public float Percentile(float percentile) => VectorStatistics.Percentile(vector, percentile);
+
+		public float Median() => VectorStatistics.Median(vector);
+
+		public float Quartile1() => VectorStatistics.Quartile1(vector);
+
+		public float Quartile3() => VectorStatistics.Quartile3(vector);
+
+		public float Iqr() => VectorStatistics.Iqr(vector);
+
+		public float PercentileX(float percentile) => VectorStatistics.PercentileX(vector, percentile);
+
+		public float MedianX() => VectorStatistics.MedianX(vector);
+
+		public float Quartile1X() => VectorStatistics.Quartile1X(vector);
+
+		public float Quartile3X() => VectorStatistics.Quartile3X(vector);
+
+		public float IqrX() => VectorStatistics.IqrX(vector);
+
 		public Vector ReduceOPX(Vector matrix, Operations operation) =>
 			VectorStatistics.ReduceOPX(vector, matrix, operation);
 	}
@@ -267,6 +373,40 @@ public static class StatisticsModule
 		public int Range() => VectorStatistics.Range(vector);
 
 		public bool All() => VectorStatistics.All(vector);
+
+		public float MeanX() => VectorStatistics.MeanX(vector);
+
+		public float VarX() => VectorStatistics.VarX(vector);
+
+		public float StdX() => VectorStatistics.StdX(vector);
+
+		public int MinX() => VectorStatistics.MinX(vector);
+
+		public int MaxX() => VectorStatistics.MaxX(vector);
+
+		public int RangeX() => VectorStatistics.RangeX(vector);
+
+		public bool AllX() => VectorStatistics.AllX(vector);
+
+		public float Percentile(float percentile) => VectorStatistics.Percentile(vector, percentile);
+
+		public float Median() => VectorStatistics.Median(vector);
+
+		public float Quartile1() => VectorStatistics.Quartile1(vector);
+
+		public float Quartile3() => VectorStatistics.Quartile3(vector);
+
+		public float Iqr() => VectorStatistics.Iqr(vector);
+
+		public float PercentileX(float percentile) => VectorStatistics.PercentileX(vector, percentile);
+
+		public float MedianX() => VectorStatistics.MedianX(vector);
+
+		public float Quartile1X() => VectorStatistics.Quartile1X(vector);
+
+		public float Quartile3X() => VectorStatistics.Quartile3X(vector);
+
+		public float IqrX() => VectorStatistics.IqrX(vector);
 
 		public VectorInt ReduceOPX(VectorInt matrix, Operations operation) =>
 			VectorStatistics.ReduceOPX(vector, matrix, operation);
