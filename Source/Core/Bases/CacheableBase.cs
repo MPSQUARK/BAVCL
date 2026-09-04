50384d7d8c499b0e44cc0fb5373a9c2db87a830a
marcel <marcelpawelczyk7@gmail.com>
2026-09-04 16:03:10 +0100

    CachableBase bug fix

--- Source/Core/Bases/CacheableBase.cs
+++ Source/Core/Bases/CacheableBase.cs
@@ -197,9 +197,14 @@ public abstract class CacheableBase<T> : ICacheable<T> where T : unmanaged
 		{
 			Residence current = Residence;
 			if (!ResidenceHelper.IsActiveCpu(current))
-				ResidenceScopeHelper.TransitionOrReconcile(this, current, Residence.ActiveCpu);
+			{
+				// Pull while GPU is still exclusive authority. Transitioning to ActiveCpu first
+				// makes IsCpuAuthority true, so SyncCPU would skip the pull and leave stale host storage.
+				if (ResidenceHelper.IsGpuAuthority(current))
+					SyncCPU();
 
-			SyncCPU();
+				ResidenceScopeHelper.TransitionOrReconcile(this, Residence, Residence.ActiveCpu);
+			}
 		}
 		catch
 		{
